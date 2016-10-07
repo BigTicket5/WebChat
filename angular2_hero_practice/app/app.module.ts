8acd19960eafb7bed8363cae33901f12334dfbe4
zh <355245849@qq.com>
2016-10-07 00:04:16 -0400

    angular tutorial practice

---
+++ angular2_hero_practice/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule}     from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent }      from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent }  from './heroes-search.component';
+import { HeroService } from './hero.service'
+import { routing } from './app.routing';
+
+@NgModule({
+  imports: [ BrowserModule,
+  			 FormsModule,
+  			 HttpModule,
+  			 InMemoryWebApiModule.forRoot(InMemoryDataService),
+  			 routing ],
+  declarations: [ AppComponent,
+  				  HeroesComponent,
+  				  DashboardComponent,
+  				  HeroDetailComponent,
+  				  HeroSearchComponent],
+  providers: [HeroService],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
