954a02abb03e5b7ead34fb349423e659fef21d46
Zhenzhong Mao <dkrose.random@gmail.com>
2016-12-19 23:40:09 -0500

    1

--- client/src/reducers/flashMessages.js
+++ client/src/reducers/flashMessages.js
@@ -1,4 +1,5 @@
 import {ADD_FLASH_MESSAGE} from '../constants/ActionTypes';
+import {DEL_FLASH_MESSAGE} from '../constants/ActionTypes';
 import shortid from 'shortid';
 
 const initialState = {
@@ -12,6 +13,8 @@ export default(state=initialState,action) =>{
 	switch (action.type){
 		case ADD_FLASH_MESSAGE:
 			return Object.assign({}, state, {id: shortid.generate(),type:action.message.type,text:action.message.text,loggedIn:true,token:action.message.token});
+		case DEL_FLASH_MESSAGE:
+		    return Object.assign({}, state, {id: null,type:null,text:null,loggedIn:false,token:null});	
 	}
 	return state;
 }
